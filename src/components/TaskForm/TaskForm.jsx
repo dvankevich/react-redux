ec876ad60b555912da72b6e688fd1a38d0e92310
dima <dvankevich@gmail.com>
2024-11-18 12:02:41 +0200

    actions

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,10 +1,12 @@
+import { useDispatch } from 'react-redux';
 import { Button } from '../Button/Button';
 import css from './TaskForm.module.css';
 
 export const TaskForm = () => {
+  const dispatch = useDispatch();
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const form = event.target;
     form.reset();
   };
 
