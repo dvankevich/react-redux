61df224da16d2ed06a2b6541ec7a06d5603ce4b6
dvankevich <dvankevich@gmail.com>
2024-11-18 18:39:37 +0200

    slices files

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,9 +1,9 @@
-import { createAction } from '@reduxjs/toolkit';
+// import { createAction } from '@reduxjs/toolkit';
 
-export const addTask = createAction('tasks/addTask');
+// export const addTask = createAction('tasks/addTask');
 
-export const deleteTask = createAction('tasks/deleteTask');
+// export const deleteTask = createAction('tasks/deleteTask');
 
-export const toggleCompleted = createAction('tasks/toggleCompleted');
+// export const toggleCompleted = createAction('tasks/toggleCompleted');
 
-export const setStatusFilter = createAction('filters/setStatusFilter');
+// export const setStatusFilter = createAction('filters/setStatusFilter');
